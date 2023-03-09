87c387b082c68200320b51d6c47cd7c65de26186
GUSTAVO PEREIRA OLIVEIRA LIMA <gustavo.00000844518@unicap.br>
2023-03-09 15:20:06 +0000

    ideas

---
+++ Passenger.java
@@ -0,0 +1,7 @@
+public class Passenger {
+    // name
+    // address
+    // rate
+    // hasanydebt
+    // age
+}
---
+++ Ride.java
@@ -0,0 +1,11 @@
+public class Ride {
+    // distance
+    // valuebydistance
+    // ifbiggerthan25kmcancel
+    // tip
+
+    //driver
+    // name
+    // typeofcar
+    // score
+}
